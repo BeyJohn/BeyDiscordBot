6586f89c1cd0b4114f48de63004f0a157d2e2a45
BeyJohn <Beyjohn1@gmail.com>
2017-06-20 00:50:54 -0500

    Update

--- BeyDiscordBot/Modules/Test.cs
+++ BeyDiscordBot/Modules/Test.cs
@@ -1,25 +1,38 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Commands;
 using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Data;
 using System.Data.SQLite;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 
 namespace BeyDiscordBot.Modules
 {
 	public class Test : ModuleBase<SocketCommandContext>
 	{
-		private SQLiteConnection _con = new SQLiteConnection($"Data Source = dbBot.sqlite;Version=3;PRAGMA foreign_keys = ON");
 
 		[Command("help"), Summary("I will tell you about all of my commands.")]
 		public async Task Help()
 		{
-			await Context.Channel.SendMessageAsync("help  - I will tell you about all of my commands.\nroll amount sides - I will roll dice for you.\nscience - This is BeyJohn's explanation for pretty much anything.\nlistReminders - I will list the reminders I have set.\nremind key - I will remind you of something you told me before.\naddReminder key message - I will remember something for you.\nremoveReminder key -I will forget some");
+            EmbedBuilder eb = new EmbedBuilder();
+
+            MethodInfo[] mi = GetType().GetMethods();
+            for (int o = 0; o < mi.Length - 5; o++)
+            {
+                CommandAttribute myAttribute1 = mi[o].GetCustomAttributes(true).OfType<CommandAttribute>().FirstOrDefault();
+                SummaryAttribute myAttribute2 = mi[o].GetCustomAttributes(true).OfType<SummaryAttribute>().FirstOrDefault();
+                eb.AddField(myAttribute1.Text, myAttribute2.Text);
+            }
+
+            await ReplyAsync("", false, eb);
 		}
 
 		[Command("roll"), Summary("I will roll dice for you.")]
@@ -32,7 +45,7 @@ namespace BeyDiscordBot.Modules
 				for (int i = 0; i < d.Length; i++)
 					d[i] = r.Next(sides) + 1;
 
-				await Context.Channel.SendMessageAsync(string.Join(", ", d) + "\nTotal roll: " + d.Sum());
+				await ReplyAsync(string.Join(", ", d) + "\nTotal roll: " + d.Sum());
 			}
 		}
 
@@ -42,7 +55,7 @@ namespace BeyDiscordBot.Modules
 			Bitmap b = Properties.Resources.science;
 			Stream s = new MemoryStream();
 
-			b.Save(s, ImageFormat.Jpeg);
+			b.Save(s, System.Drawing.Imaging.ImageFormat.Jpeg);
 			s.Seek(0, SeekOrigin.Begin);
 
 			await Context.Channel.SendFileAsync(s, "science.jpg");
@@ -51,99 +64,69 @@ namespace BeyDiscordBot.Modules
 		[Command("listReminders"), Summary("I will list the reminders I have set.")]
 		public async Task ListReminders()
 		{
-			DataSet ds = await FillDataSet($"SELECT * FROM Reminders", "Reminders");
+			DataSet ds = await Program.FillDataSet($"SELECT * FROM Reminders", "Reminders");
 			if (ds.Tables[0].Rows.Count > 0)
 			{
-				await Context.Channel.SendMessageAsync(string.Join(", ", ds.Tables[0].Rows.OfType<DataRow>().Select(r => r[0].ToString().Replace("###", "'"))));
+				await ReplyAsync(string.Join(", ", ds.Tables[0].Rows.OfType<DataRow>().Select(r => r[0].ToString().Replace("###", "'"))));
 			}
 		}
 
 		[Command("remind"), Summary("I will remind you of something you told me before.")]
 		public async Task Remind(string key)
 		{
-			DataSet ds = await FillDataSet($"SELECT * FROM Reminders WHERE key = '{key.Replace("'", "###")}'", "Reminders");
+			DataSet ds = await Program.FillDataSet($"SELECT * FROM Reminders WHERE key = '{key.Replace("'", "###")}'", "Reminders");
 			if (ds.Tables[0].Rows.Count > 0)
 			{
 				DataRow dr = ds.Tables[0].Rows[0];
-				await Context.Channel.SendMessageAsync(dr["message"].ToString().Replace("###", "'"));
+				await ReplyAsync(dr["message"].ToString().Replace("###", "'"));
 			}
 		}
 
 		[Command("addReminder"), Summary("I will remember something for you.")]
 		public async Task AddReminder(string key, string message)
 		{
-			DataSet ds = await FillDataSet($"SELECT * FROM Reminders WHERE key = '{key.Replace("'", "###")}'", "Reminders");
+			DataSet ds = await Program.FillDataSet($"SELECT * FROM Reminders WHERE key = '{key.Replace("'", "###")}'", "Reminders");
 			if (ds.Tables[0].Rows.Count > 0)
 			{
-				SQLiteCommand cmd = _con.CreateCommand();
+				SQLiteCommand cmd = Program._con.CreateCommand();
 				cmd.CommandText = "UPDATE Reminders SET [message] = @message WHERE [key] = @key;";
 				cmd.Parameters.AddWithValue("@message", message.Replace("'", "###"));
 				cmd.Parameters.AddWithValue("@key", key.Replace("'", "###"));
 
-				await ExecuteCommand(cmd);
+				await Program.ExecuteCommand(cmd);
 			}
 			else
 			{
-				SQLiteCommand cmd = _con.CreateCommand();
+				SQLiteCommand cmd = Program._con.CreateCommand();
 				cmd.CommandText = $"INSERT INTO Reminders([key],[message])Values('{key.Replace("'", "###")}','{message.Replace("###", "'")}')";
-				await ExecuteCommand(cmd);
+				await Program.ExecuteCommand(cmd);
 			}
 		}
 
 		[Command("removeReminder"), Summary("I will forget something I was told.")]
 		public async Task RemoveReminder(string key)
 		{
-			SQLiteCommand cmd = _con.CreateCommand();
+			SQLiteCommand cmd = Program._con.CreateCommand();
 
 			cmd.CommandText = "DELETE FROM Reminders WHERE [key] = @key";
 			cmd.Parameters.AddWithValue("@key", key.Replace("'", "###"));
 
-			await ExecuteCommand(cmd);
+			await Program.ExecuteCommand(cmd);
 		}
 
-		public async Task<DataSet> FillDataSet(string sql, string tableName)
-		{
-			DataSet ds = new DataSet();
+        [Command("shutdown"), Summary("Only BeyJohn may use this command.")]
+        public async Task Shutdown()
+        {
+            if (Context.User.Id == 120706726042402816)
+            {
+                Program.IsExit = true;
+                await ReplyAsync("Shutting Down ...");
+            }
+            else
+            {
+                await ReplyAsync("You can't shut me down!");
+            }
+        }
 
-			await Task.Run(() =>
-			{
-				try
-				{
-					SQLiteDataAdapter da = new SQLiteDataAdapter(sql, _con);
-					da.Fill(ds, tableName);
-				}
-				catch (Exception ex)
-				{
-					Console.WriteLine(ex.Message);
-				}
-				finally { _con.Close(); }
-			});
-			return ds;
-		}
-
-		private async Task<bool> ExecuteCommand(params SQLiteCommand[] commands)
-		{
-			bool success = false;
-			await Task.Run(() =>
-			{
-				try
-				{
-					_con.Open();
-					foreach (SQLiteCommand command in commands)
-					{
-						command.Connection = _con;
-						command.Prepare();
-						command.ExecuteNonQuery();
-					}
-					success = true;
-				}
-				catch (Exception ex)
-				{
-					Console.WriteLine(ex.Message);
-				}
-				finally { _con.Close(); }
-			});
-			return success;
-		}
 	}
 }
--- BeyDiscordBot/Program.cs
+++ BeyDiscordBot/Program.cs
@@ -3,14 +3,19 @@ using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
 using System.IO;
+using System.Data;
+using System.Data.SQLite;
 
 namespace BeyDiscordBot
 {
 	internal class Program
 	{
-		private string _botToken = Properties.Resources.Token;
+
+        public static SQLiteConnection _con = new SQLiteConnection($"Data Source = dbBot.sqlite;Version=3;PRAGMA foreign_keys = ON");
+        private string _botToken = Properties.Resources.Token;
 		private DiscordSocketClient _client;
 		private CommandHandler _handler;
+        public static bool IsExit = false;
 
 		private static void Main(string[] args)
 		=> new Program().StartAsync().GetAwaiter().GetResult();
@@ -27,7 +32,10 @@ namespace BeyDiscordBot
 			await _client.LoginAsync(TokenType.Bot, _botToken);
 			await _client.StartAsync();
 
-			await Task.Delay(-1);
+            while(!IsExit)
+			    await Task.Delay(1000);
+
+            await _client.LogoutAsync();
 		}
 
 		private void VerifyDatabase()
@@ -43,5 +51,51 @@ namespace BeyDiscordBot
 			Console.WriteLine(msg.ToString());
 			return Task.CompletedTask;
 		}
-	}
+
+        public static async Task<DataSet> FillDataSet(string sql, string tableName)
+        {
+            DataSet ds = new DataSet();
+
+            await Task.Run(() =>
+            {
+                try
+                {
+                    SQLiteDataAdapter da = new SQLiteDataAdapter(sql, _con);
+                    da.Fill(ds, tableName);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally { _con.Close(); }
+            });
+            return ds;
+        }
+
+        public static async Task<bool> ExecuteCommand(params SQLiteCommand[] commands)
+        {
+            bool success = false;
+            await Task.Run(() =>
+            {
+                try
+                {
+                    _con.Open();
+                    foreach (SQLiteCommand command in commands)
+                    {
+                        command.Connection = _con;
+                        command.Prepare();
+                        command.ExecuteNonQuery();
+                    }
+                    success = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally { _con.Close(); }
+            });
+            return success;
+        }
+
+    }
 }
